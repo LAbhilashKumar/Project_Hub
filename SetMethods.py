6357210ea6ea4ec9e0d9c8f3b4744663b952a609
LAbhilashKumar <160405976+LAbhilashKumar@users.noreply.github.com>
2024-02-18 22:06:48 +0530

    Add files via upload

---
+++ SetMethods.py
@@ -0,0 +1,13 @@
+#union
+set1={1,2,3}
+set2={3,4,5}
+print("union :",set1.union(set2))
+
+# intersection
+print("intersection :",set1.intersection(set2))
+
+# difference
+print("diff :",set1.difference(set2))
+
+# symmetric difference
+print("symmetric difference :",set1.symmetric_difference(set2))
