b229e8214ee9c9b80816d67b160f4402b00a9726
LAbhilashKumar <160405976+LAbhilashKumar@users.noreply.github.com>
2024-02-18 21:59:00 +0530

    Add files via upload

---
+++ printing_positive_numbers.py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if i >=0:
+        print(i)
+    else:
+        pass
+print("list 2 ")
+list2=[12,14,-95,3]
+for i in list2:
+    if i >= 0:
+        print(i)
+    else:
+        pass
