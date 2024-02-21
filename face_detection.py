7bebc6a22c19cbb28d29206af7a3fa50dc032e45
Abhilash <160405976+LAbhilashKumar@users.noreply.github.com>
2024-02-21 06:43:40 +0530

    Add files via upload
     designed to detect faces in real-time using  webcam

---
+++ face_detection.py
@@ -0,0 +1,25 @@
+#  designed to detect faces in real-time using  webcam
+
+import cv2
+import time
+face_cap=cv2.CascadeClassifier("C:/ProgramFiles/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml")
+video_cap=cv2.VideoCapture(0)
+key="a"
+while True:
+    ret, video_data=video_cap.read()
+    col=cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
+    faces=face_cap.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(video_data,(x,y),(x+w,y+h),(0,256,0),2)
+    cv2.imshow("Video_live",video_data)
+    if cv2.waitKey(10)==ord('a'):
+        break
+video_cap.release()
+cv2.destroyAllWindows()
+
